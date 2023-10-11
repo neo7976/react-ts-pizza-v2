6a4728b37f877bd4d165c4aa18202f8c8601798c
SobinDA <SobinDA@srgroup.ru>
2023-10-11 11:13:01 +0300

    dropDown

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,8 +4,11 @@ import {useDebounce} from "../hooks/debounce";
 
 export function HomePage() {
     const [search, setSearch] = useState('');
+    const [dropDown, setDropDown] = useState(false);
     const debounce = useDebounce(search)
-    const {isLoading, isError, data} = useSearchUsersQuery('neo');
+    const {isLoading, isError, data} = useSearchUsersQuery(debounce, {
+        skip: debounce.length < 3 // Не отправляем запрос, если он меньше 3 символов
+    });
     // console.log(data);
 
     /*
@@ -17,7 +20,8 @@ export function HomePage() {
 
     useEffect(() => {
         console.log(debounce)
-    }, [debounce])
+        setDropDown(debounce.length > 3 && data?.length! > 0)
+    }, [debounce, data])
 
     return (
         <div className={'flex justify-center pt-10 mx-auto h-screen w-screen'}>
@@ -31,9 +35,18 @@ export function HomePage() {
                     onChange={e => setSearch(e.target.value)}
                 />
 
-                <div className={'absolute top-[42[x] left-0 right-0 max-h-[200px] shadow-md bg-white'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, unde.
-                </div>
+                {dropDown &&
+                    <ul className={'list-none absolute top-[42[x] left-0 right-0 max-h-[200px] bg-white overflow-y-scroll shadow-md '}>
+                        {isLoading && <p className={'text-center'}>Loading...</p>}
+                        {data?.map(user => (
+                            <li
+                                key={user.id}
+                                className={'py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'}
+                            >
+                                {user.login}
+                            </li>
+                        ))}
+                    </ul>}
             </div>
 
         </div>
