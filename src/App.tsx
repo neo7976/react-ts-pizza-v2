57cfb9c1149cf45e620a16175ba5b40ea77c4c90
Dmitrii Sobin <sobinda@srgroup.ru>
2023-10-17 12:58:30 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import {Route, Routes} from "react-router-dom";
-import {HomePage} from "./pages/HomePage";
-import {FavoritesPage} from "./pages/FavoritesPage";
-import {Navigation} from "./components/Navigation";
 
 
 function App() {
 
     return (
         <>
-            <Navigation/>
-            <Routes>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='/favourites' element={<FavoritesPage/>}/>
-            </Routes>
+            <div>Hello</div>
         </>
 
     )
