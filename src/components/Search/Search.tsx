fbe10d262c074d9f3048106854b472270146f37f
SobinDA <SobinDA@srgroup.ru>
2023-10-25 11:58:22 +0300

    добавил свои иконки из react-icon

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './Search.module.scss'
+import {GrClose} from "react-icons/gr";
+import {BsSearch} from "react-icons/bs";
+
 
 interface SearchProps {
     search: string,
@@ -9,26 +12,7 @@ interface SearchProps {
 const Search = ({search, setSearch}: SearchProps) => {
     return (
         <div className={styles.root}>
-            <svg
-                className={styles.icon}
-                xmlns="http://www.w3.org/2000/svg"
-                enable-background="new 0 0 64 64" height="64px" id="Layer_1" version="1.1" viewBox="0 0 64 64"
-                width="64px">
-                <g>
-                    <circle cx="32" cy="25" fill="none" r="16" stroke="#4D4D4D" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/>
-                    <line
-                        fill="none" stroke="#4D4D4D" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-miterlimit="10"
-                        x1="40.9" x2="38.8" y1="44.1" y2="39.5"/>
-                    <line fill="none" stroke="#4D4D4D" stroke-linecap="round"
-                          stroke-linejoin="round" stroke-miterlimit="10"
-                          stroke-width="4" x1="40.9" x2="46" y1="44.1" y2="55"/>
-                    <path
-                        d="M36.2,13.7   c2.7,1,4.9,2.9,6.3,5.3" fill="none" stroke="#4D4D4D" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-miterlimit="10"/>
-                </g>
-            </svg>
+            <div className={styles.icon}><BsSearch/></div>
             <input
                 className={styles.input}
                 type="text"
@@ -36,16 +20,13 @@ const Search = ({search, setSearch}: SearchProps) => {
                 value={search}
                 onChange={(event) => setSearch(event.target.value)}
             />
-            {search && <svg
-                className={styles.clearIcon}
-                onClick={() => setSearch('')}
-                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <g>
-                    <path d="M0 0h24v24H0z" fill="none"/>
-                    <path
-                        d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/>
-                </g>
-            </svg>}
+            {search &&
+                <div
+                    className={styles.clearIcon}
+                    onClick={() => setSearch('')}
+                ><GrClose/>
+                </div>
+            }
         </div>
     );
 };
